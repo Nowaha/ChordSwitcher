2921e38990044e046a1b840407f6bba3e5458f1e
Noah Verkaik <noahverkaik@gmail.com>
2024-11-01 23:53:27 +0100

    Use hook system, fix not cloning, save on first open

--- gd/mods/ChordSwitcher/main.gd
+++ gd/mods/ChordSwitcher/main.gd
@@ -1,12 +1,11 @@
 class_name ChordSwitcher
 extends Node
 
-var file_path = "user://custom_chords.json"
-var saved = []
-var selected_index = 0
-
-func l(message: String):
-	print("ChordSwitcher: " + message)
+var file_path: String = "user://custom_chords.json"
+var ever_saved: bool = false
+var saved: Array = []
+var selected_index: int = 0
+var guitar: Node = null
 
 func _ready():
 	var i = 0
@@ -24,66 +23,63 @@ func _ready():
 		])
 		i += 1
 		
-	load_chords_from_file()
+	ever_saved = load_chords_from_file()
+
 	l("Ready!")
+	
+func _inject(guitar_node: Node) -> void:
+	l("Injecting guitar")
+	guitar = guitar_node
+	
+	if ever_saved:
+		guitar.saved_shapes = saved[selected_index].duplicate()
+		refresh_guitar(guitar)
+	else:
+		saved[0] = guitar.saved_shapes.duplicate()
+		ever_saved = save_chords_to_file()
+	
+	guitar.connect("tree_exiting", self, "_guitar_closed")
 
-func findGuitarNode() -> Node:
-	return find_node_with_script(get_tree().root, "res://Scenes/Minigames/Guitar/guitar_minigame.gd")
-	
-func find_node_with_script(root: Node, script_path: String) -> Node:
-	if root.get_script() and root.get_script().resource_path == script_path:
-		return root
-	
-	for child in root.get_children():
-		var found = find_node_with_script(child, script_path)
-		if found:
-			return found
-	
-	return null
-
-func notify(message: String, code: int = 0) -> void:
-	l(message)
-	PlayerData._send_notification(message, code)
+func _guitar_closed():
+	l("Disconnecting from guitar")
+	guitar = null
 
 func refresh_guitar(guitar: Node):
-	if guitar != null:
-		guitar._select_shape(guitar.selected_shape)
+	guitar._select_shape(guitar.selected_shape)
 
 func ld(index: int) -> void:
-	var guitar = findGuitarNode()
-	
 	if guitar == null:
 		notify("Failed to find guitar", 1)
 		return
 
 	selected_index = index
-	guitar.saved_shapes = saved[index]
+	guitar.saved_shapes = saved[index].duplicate()
 	refresh_guitar(guitar)
 	
 	notify("Loading from " + str(index + 1))
 
 func sv(index: int) -> void:
-	var guitar = findGuitarNode()
-	
 	if guitar == null:
 		notify("Failed to find guitar!", 1)
 		return
 	
-	saved[index] = guitar.saved_shapes
+	saved[index] = guitar.saved_shapes.duplicate()
 	
 	notify("Saving in " + str(index + 1))
 
-func save_chords_to_file() -> void:
+func save_chords_to_file() -> bool:
 	var file = File.new()
 	if file.open(file_path, File.WRITE) == OK:
 		var json = to_json(saved)
 		file.store_string(json)
 		file.close()
 		notify("Saved chords to file!")
+		return true
 	else:
 		notify("Failed to save chords", 1)
+		return false
 		
-func load_chords_from_file() -> void:
+func load_chords_from_file() -> bool:
 	var file = File.new()
 	if file.open(file_path, File.READ) == OK:
 		var json = file.get_as_text()
@@ -91,14 +87,18 @@ func load_chords_from_file() -> void:
 		file.close()
 		notify("Loaded chords from file!")
 		
-		var guitar = findGuitarNode()
 		if guitar != null:
-			guitar.saved_shapes = saved[selected_index]
+			guitar.saved_shapes = saved[selected_index].duplicate()
 			refresh_guitar(guitar)
+		
+		return true
 	else:
 		notify("Failed to load chords", 1)
+		return false
 
 func _input(event):
+	if guitar == null: return
+	
 	if event is InputEventKey and event.pressed:
 		if event.scancode >= KEY_F1 and event.scancode <= KEY_F9:
 			var i = event.scancode - KEY_F1
@@ -110,3 +110,15 @@ func _input(event):
 			load_chords_from_file()
 		elif event.scancode == KEY_F11 and event.shift:
 			save_chords_to_file()
+
+
+# Utils
+func l(message: String):
+	print("ChordSwitcher: " + message)
+
+func findGuitarNode() -> Node:
+	return get_node("/root/playerhud/guitar")
+
+func notify(message: String, code: int = 0) -> void:
+	l(message)
+	PlayerData._send_notification(message, code)
